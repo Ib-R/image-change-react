1144db5f167f801e092aafac22c37273f3fd49bb
IBR <business1232@gmail.com>
2020-08-06 20:31:16 +0200

    Updates

---
+++ precache-manifest.ce033b109bfa63eacb8d5268b9c45ab1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24a63f6b6d3712f1db2e72b8d8a98c57",
+    "url": "/image-change-react/index.html"
+  },
+  {
+    "revision": "888e0512152728352cd3",
+    "url": "/image-change-react/static/css/main.c17dfced.chunk.css"
+  },
+  {
+    "revision": "409684de606170404c46",
+    "url": "/image-change-react/static/js/2.83440a60.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/image-change-react/static/js/2.83440a60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "888e0512152728352cd3",
+    "url": "/image-change-react/static/js/main.343e9e3e.chunk.js"
+  },
+  {
+    "revision": "f060d725699466af92da",
+    "url": "/image-change-react/static/js/runtime-main.b064fd4d.js"
+  },
+  {
+    "revision": "f5e60cb64c6a9c191affdf67f009ee12",
+    "url": "/image-change-react/static/media/2.f5e60cb6.jpg"
+  },
+  {
+    "revision": "ea0662141b07a76014a0cf15500c3a8e",
+    "url": "/image-change-react/static/media/3.ea066214.jpg"
+  }
+]);
